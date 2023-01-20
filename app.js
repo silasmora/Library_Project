b66ab8fec8c439426efd6d85c558ae17cbd39a1a
Silas Morales <silas.mora22@gmail.com>
2023-01-20 14:52:52 -0800

    moved elements consts outside of library class

--- app.js
+++ app.js
@@ -2,3 +2,42 @@ console.log("Hello World!\n==========\n");
 
 // PROJECT Section
 console.log("PROJECT:\n==========\n");
+
+const titleEl = document.getElementById("title")
+const authorEl = document.getElementById("author")
+const readEl = document.getElementById("read")
+
+class Book {
+  constructor(title, author, read){
+    this.id = id
+    this.title = title
+    this.author = author
+    this.read = read
+  }
+}
+
+class Library {
+  constructor(bookCount, books) {
+    this.bookCount = bookCount
+    this.books = books
+  }
+  markRead(checkbox, id) {
+    for (let i = 0; i < this.books.length; i++){
+      if (id === this.books.id) {
+        books.read = true
+        checkbox.checked = true
+        checkbox.disabled = true
+    }
+  }
+  }
+
+
+  addBook() {
+
+    const newBook = new Book(titleEl.value, authorEl.value, readEl.value)
+    this.book.push(newBook)
+
+    const newTabler = document.createElement("tr")
+  }
+
+}
